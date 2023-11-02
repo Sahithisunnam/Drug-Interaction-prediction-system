d41c07a5f102319d6d0e32614757cf516d605bd9
Sahithisunnam <129082406+Sahithisunnam@users.noreply.github.com>
2023-11-02 18:28:27 +0530

    FRONTEND

---
+++ frontend/templates/script.js
@@ -0,0 +1,19 @@
+function authenticate() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Implement authentication logic here (backend).
+}
+
+function register() {
+    const newUsername = document.getElementById('newUsername').value;
+    const newPassword = document.getElementById('newPassword').value;
+
+    // Implement registration logic here (backend).
+}
+
+function uploadDataset() {
+    const dataset = document.getElementById('dataset').files[0];
+
+    // Implement dataset upload logic here (backend).
+}
